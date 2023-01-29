e1d83a5bbaabb626865459a73c7ed58e876148f0
Grzegorz Gurgul <ggurgul@microsoft.com>
2023-01-29 21:10:25 +0100

    First tests

---
+++ GitTreeFilter.Core.Tests/SolutionRepositoryTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitTreeFilter.Core.Tests
+{
+    [TestClass]
+    public class SolutionRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
