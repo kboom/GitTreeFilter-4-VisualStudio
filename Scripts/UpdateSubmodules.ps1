f8f05ac9a7a7094b03262efceeba8eed06adda22
Grzegorz Gurgul <ggurgul@microsoft.com>
2024-12-24 13:47:40 +0100

    Add module

--- Scripts/UpdateSubmodules.ps1
+++ Scripts/UpdateSubmodules.ps1
@@ -4,5 +4,7 @@ git submodule update --init --remote
 git submodule foreach git pull origin main
 git submodule foreach git checkout main
 
-Set-Location "$PSScriptRoot/TestResources/TestRepository"
-git checkout feature
+$TestResourcesDir = Join-Path $PSScriptRoot "../TestResources"
+
+git --git-dir (Join-Path $TestResourcesDir "TestRepository") checkout feature
+git --git-dir (Join-Path $TestResourcesDir "TestRepository2") checkout feature
