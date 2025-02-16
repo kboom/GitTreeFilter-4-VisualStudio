5d33c06b51ec697acf9999cadfbf5e9beb3db5b4
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2025-02-16 22:19:13 +0100

    Fix missing buttons (#62)
    Co-authored-by: Grzegorz Gurgul <ggurgul@microsoft.com>

---
+++ GitTreeFilter/GitTreeFilterOutput.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+
+public static class GitTreeFilterOutput
+{
+    private static IVsOutputWindowPane _outputPane;
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        ThreadHelper.ThrowIfNotOnUIThread();
+        if (_outputPane == null)
+        {
+            var outputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            if (outputWindow != null)
+            {
+                Guid customPaneGuid = Guid.NewGuid();
+                outputWindow.CreatePane(ref customPaneGuid, nameof(GitTreeFilter), 1, 1);
+                outputWindow.GetPane(ref customPaneGuid, out _outputPane);
+            }
+        }
+    }
+
+    public static void WriteLine(string message)
+    {
+        ThreadHelper.ThrowIfNotOnUIThread();
+
+        if (_outputPane != null)
+        {
+            _outputPane.OutputStringThreadSafe(message + Environment.NewLine);
+        }
+    }
+}
